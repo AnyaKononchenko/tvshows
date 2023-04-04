d3217dba19e2e89d270bb7b85f49426b01668505
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-04-04 10:49:38 +0200

    the basic func is done

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Used packages:
  - dotenv: to get access to the enviroment variables;
  - [multer](https://www.npmjs.com/package/multer): file uploading (formidable is an alternative);
  - [express-validator](https://www.npmjs.com/package/express-validator): perform validation (joi is an alternative)
+ - mongoose : to connect with MongoDB
 
 
 
@@ -18,3 +19,7 @@ Used packages:
 Middleware is cool! You can add all needed validation to it, without complicating thr function which actually handles request. Also you can add multiple middleware functions to separate logic (wow) :B)  
 
 Validation is interesting :)
+
+Documentation:
+ - [swagger](https://swagger.io/docs/specification/about/)
+ - [online editor](https://editor.swagger.io/)
--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const dev = require('./config');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const router = express.Router();
+const cors = require("cors");
+
 const tvshowsRouter = require('./routes/tvshows');
 const notFound = require("./controllers/notFound");
-const router = express.Router();
 
 
 const app = express();
-const port = dev.env.port;
+const port = dev.port;
 
+app.use(cors);
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
@@ -22,10 +25,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/public/html/index.html')
 });
 
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).sent('Something broke!');
-// })
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  if (err) return res.status(500).sent('Something broke!');
+})
 
 router.use('*', notFound);
 
